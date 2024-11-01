f99f52641b39b716ec0ce9612ed2745d60c3e6ad
ericpo1sh <ericpo1sh@gmail.com>
2024-11-01 13:08:17 -0500

    ready to seed data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-"use client";
-
-import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
+import Filters from "@/components/Filters";
 
 export default function Page() {
 
   return (
     <div className="flex h-screen w-full text-white" style={{ backgroundColor: '#00003c' }}>
-      hello world
+      <Filters/>
     </div>
   );
 };
---
+++ components/Filters.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useState, useEffect } from 'react';
+
+const Filters: React.FC = () => {
+
+  return (
+    <div>Filters</div>
+  );
+};
+
+export default Filters;
